b0494d211bf242746d9baedd06c7b8339b2e9cd1
aleks3650 <94302102+aleks3650@users.noreply.github.com>
2024-09-29 17:01:25 +0200

    third  commit

--- frontend/src/lib/axios.jsx
+++ frontend/src/lib/axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5000/api/products/",
+  baseURL: "https://firstfullstack-pv32.onrender.com/",
   headers: {
     "Content-Type": "application/json",
     timeout: 1000,
